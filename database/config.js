b8c0484501935281f8ca2e336cdf12831c806fc1
netomachado <netomachado72@gmail.com>
2021-08-15 11:34:36 -0300

    iniciando um projeto com sequelize

---
+++ database/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    development: {
+      username: "root",
+      password: '',
+      database: "iotusers",
+      host: "localhost",
+      dialect: "mysql"
+    },
+    test: {
+      username: "root",
+      password: '',
+      database: "iotusers",
+      host: "localhost",
+      dialect: "mysql"
+    },
+    production: {
+      username: "root",
+      password: '',
+      database: "iotusers",
+      host: "localhost",
+      dialect: "mysql"
+    }
+  }
